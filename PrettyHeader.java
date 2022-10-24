0ce9ce43f6cadf93697157677758b0b1e0f5e7e6
ChaseHoffmann15 <98759157+ChaseHoffmann15@users.noreply.github.com>
2022-10-24 15:34:42 -0500

    Add files via upload

---
+++ PrettyHeader.java
@@ -0,0 +1,18 @@
+public class PrettyHeader {
+}
+    public static void main(String[] args) {
+
+
+
+        String header = SafeInputs.prettyHeader("COMPUTER PROGRAMMING I");
+
+
+
+
+
+
+
+
+
+    }
+}
